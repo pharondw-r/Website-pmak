79a25fc0311ec9fe1e32fa58ac0605e96b92a48a
pharondw-r <pharondw@gmail.com>
2026-05-25 23:01:36 +0400

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import './i18n'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
